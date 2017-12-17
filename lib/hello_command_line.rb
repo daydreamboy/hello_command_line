9faece11ce32809f46de928660103bb94bf40a19
daydreamboy <wesley4chen@gmail.com>
2017-12-17 22:35:54 +0800

    add files

---
+++ lib/hello_command_line.rb
@@ -0,0 +1,9 @@
+require "hello_command_line/version"
+
+module HelloCommandLine
+  class Chatter
+    def say_hello
+      puts 'This is a command tool written by ruby.'
+    end
+  end
+end
---
+++ lib/hello_command_line/version.rb
@@ -0,0 +1,3 @@
+module HelloCommandLine
+  VERSION = "0.1.0"
+end
